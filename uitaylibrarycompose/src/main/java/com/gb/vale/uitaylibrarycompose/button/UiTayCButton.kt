bb0b37ed1978f65a18608f5988bb3cde9cc210d3
tayler <emontesinos812@gmail.com>
2024-08-03 12:06:50 -0500

    create Switch

--- uitaylibrarycompose/src/main/java/com/gb/vale/uitaylibrarycompose/button/UiTayCButton.kt
+++ uitaylibrarycompose/src/main/java/com/gb/vale/uitaylibrarycompose/button/UiTayCButton.kt
@@ -1,8 +1,17 @@
 package com.gb.vale.uitaylibrarycompose.button
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -12,9 +21,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.gb.vale.uitaylibrarycompose.R
 import com.gb.vale.uitaylibrarycompose.model.UTStyleCButton
 import com.gb.vale.uitaylibrarycompose.model.UiTayButtonModel
 import com.gb.vale.uitaylibrarycompose.model.uiTayBackground
@@ -22,6 +37,7 @@ import com.gb.vale.uitaylibrarycompose.model.uiTayStroke
 import com.gb.vale.uitaylibrarycompose.model.uiTayTextColor
 import com.gb.vale.uitaylibrarycompose.model.utBtnState
 import com.gb.vale.uitaylibrarycompose.utils.UI_TAY_TEXT_DEFAUL
+import com.gb.vale.uitaylibrarycompose.utils.uiTayNoRippleClickable
 
 
 @Composable
@@ -47,5 +63,36 @@ fun UiTayCButton(uiTayText : String = UI_TAY_TEXT_DEFAUL, uiTayEnable : Boolean 
          uiTayBtnModel.uiTayTextColor(uiTayStyleBtn,uiTayEnable.utBtnState())))
     }
 
+}
+
+fun UiTaySwitch(state : Boolean = false,tayClickItem: (Boolean) -> Unit){
+    val checked = remember { mutableStateOf(false) }
+    checked.value = state
+    Row(
+        modifier = Modifier
+            .height(30.dp)
+            .width(70.dp)
+            .padding(end = 16.dp)
+            .clip(RoundedCornerShape(30.dp))
+            .border(1.dp, Color.Magenta, CircleShape)
+            .background(Color.White).uiTayNoRippleClickable{
+                tayClickItem.invoke(!state)
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement= if(checked.value) Arrangement.End else Arrangement.Start
+
+    ) {
+        Image(
+            painter = painterResource(R.drawable.uic_tay_bg_circle),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(28.dp).padding(2.dp)
+                .clip(CircleShape)                       // clip to the circle shape
+                .border(2.dp, Color.Black, CircleShape)
+                .background(Color.Magenta) ,
+            contentDescription = "",
+            alignment = Alignment.Center
+        )
+    }
 }
 
